6d609245b2a83508bda76eadeef5ae07fa38a221
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-06-09 12:03:27 +0530

    Update Event model

--- backend/src/main/java/com/streampulse/model/Event.java
+++ backend/src/main/java/com/streampulse/model/Event.java
@@ -1,5 +1,29 @@
-package main.java.com.streampulse.model;
+package com.streampulse.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "events")
 public class Event {
-    
+
+    @Id
+    private String id;
+    private String type;        
+    private String userId;
+    private String region;
+    private Object payload;
+    private LocalDateTime timestamp;
+    private boolean anomaly;
+    private double anomalyScore;
+
+    public Event() {
+        this.timestamp = LocalDateTime.now();
+        this.anomaly = false;
+        this.anomalyScore = 0.0;
+    }
 }
