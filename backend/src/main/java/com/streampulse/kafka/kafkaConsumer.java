d821a44dab838ea4c05a3e080bca44d0cb90c19f
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-06-17 21:41:22 +0530

    fixed a bug

--- backend/src/main/java/com/streampulse/kafka/kafkaConsumer.java
+++ backend/src/main/java/com/streampulse/kafka/kafkaConsumer.java
@@ -5,14 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.streampulse.websocket.WebSocketHandler;
+
 @Service
 public class kafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(kafkaConsumer.class);
+    private final WebSocketHandler webSocketHandler;
+
+    public kafkaConsumer(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
 
     @KafkaListener(topics = "streampulse-events", groupId = "streampulse-group")
     public void consume(String eventJson) {
         log.info("Received event from Kafka: {}", eventJson);
-        // EventService will process this in Day 3
+        webSocketHandler.broadcast(eventJson);
     }
 }
